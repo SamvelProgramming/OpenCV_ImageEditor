342f475ca8132b34cb2d022c7abf390ca2cc03f9
Gor Mkrtchyan <gorm4063@gmail.com>
2026-07-03 14:56:45 +0400

    handle all validations, remove dublicates from image_operations.cpp, optimize main in start.cpp

--- image_operations.cpp
+++ image_operations.cpp
@@ -3,44 +3,29 @@
 void applyGrey(const cv::Mat &input, cv::Mat &output)
 {
     cv::cvtColor(input, output, cv::COLOR_BGR2GRAY);
-    cv::namedWindow("Gray Image", cv::WINDOW_NORMAL);
-    cv::imshow("Gray Image", output);
 }
 void applyRotate90(const cv::Mat &input, cv::Mat &output)
 {
     cv::rotate(input, output, cv::ROTATE_90_CLOCKWISE);
-    cv::namedWindow("Rotated Image", cv::WINDOW_NORMAL);
-    cv::imshow("Rotated Image", output);
 }
 void applyBlur(const cv::Mat &input, cv::Mat &output)
 {
     cv::GaussianBlur(input, output, cv::Size(15, 15), 0, 0);
-    cv::namedWindow("Blurred Image", cv::WINDOW_NORMAL);
-    cv::imshow("Blurred Image", output);
 }
 void apply180(const cv::Mat& input ,cv::Mat& output){
     
     cv::rotate(input,output,cv::ROTATE_180);
-    cv::namedWindow("180 Rotated Image", cv::WINDOW_NORMAL);
-    cv::imshow("180 Rotated Image", output);
 }
 void applyFlip(const cv::Mat& input, cv::Mat& output){
     cv::flip(input,output,1);
-    cv::namedWindow("Flipped Image", cv::WINDOW_NORMAL);
-    cv::imshow("Flipped Image", output);
 }
 void applyResize(const cv::Mat& input, cv::Mat& output, double scaleFactor){
     cv::resize(input,output,cv::Size(),scaleFactor,scaleFactor);
-    cv::imshow("Resized Image", output);
 }
 void applyCanny(const cv::Mat& input, cv::Mat& output, double threshold1, double threshold2){
     cv::Canny(input, output, threshold1, threshold2);
-    cv::namedWindow("Canny Edge Image", cv::WINDOW_NORMAL);
-    cv::imshow("Canny Edge Image", output);
 }
 void applyMorphology(const cv::Mat& input, cv::Mat& output, int operation, int kernelSize){
     cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(kernelSize, kernelSize));
     cv::morphologyEx(input, output, operation, element);
-    cv::namedWindow("Morphology Image", cv::WINDOW_NORMAL);
-    cv::imshow("Morphology Image", output);
 }
--- start.cpp
+++ start.cpp
@@ -3,8 +3,10 @@
 #include <string>
 #include "image_operations.h"
 #include <map>
+#include <filesystem>
 #include <functional>
 
+namespace fs = std::filesystem;
 using ActionFunction = std::function<void(const cv::Mat &, cv::Mat &, int, char **)>;
 
 void Actions(const cv::Mat &input, cv::Mat &output, int argc, char **argv)
@@ -60,6 +62,12 @@ void Actions(const cv::Mat &input, cv::Mat &output, int argc, char **argv)
              {
                  int operation = std::stoi(argv[4]);
                  int kernelSize = std::stoi(argv[5]);
+                 if (kernelSize <= 0 || kernelSize % 2 == 0 || operation < 0 || operation > 3)
+                 {
+                     std::cout << "Error: Kernel size must be a positive odd integer." << std::endl;
+                     std::cout << "Error: Operation must be 0 (MORPH_ERODE), 1 (MORPH_DILATE), 2 (MORPH_OPEN), or 3 (MORPH_CLOSE)." << std::endl;
+                     return;
+                 }
                  applyMorphology(input, output, operation, kernelSize);
              }
              catch (const std::invalid_argument &e)
@@ -72,6 +80,8 @@ void Actions(const cv::Mat &input, cv::Mat &output, int argc, char **argv)
     if (it != actionMap.end())
     {
         it->second(input, output, argc, argv);
+        cv::namedWindow(it->first + " Image", cv::WINDOW_NORMAL);
+        cv::imshow(it->first + " Image", output);
     }
     else
     {
@@ -80,25 +90,57 @@ void Actions(const cv::Mat &input, cv::Mat &output, int argc, char **argv)
     }
 }
 
+bool isValidPath(const std::string &path)
+{
+    return fs::exists(path);
+}
+bool providePath(std::string &changed_image_directory)
+{
+    std::cout << "Error: The path doesn't exist." << std::endl;
+    std::cout << "Do you want to create the directory? (y/n): ";
+    char response;
+    std::cin >> response;
+    if (response != 'y' && response != 'Y')
+    {
+        std::cout << "Please provide a valid path for the changed image." << std::endl;
+        return false;
+    }
+    fs::create_directories(changed_image_directory);
+    std::cout << "Directory created: " << changed_image_directory << std::endl;
+    return true;
+}
+
+void printHelp(const std::string &programName)
+{
+    std::cout << "usage: " << programName << " <image path> <changed_image path> <action> <parameters>(if they exist)" << std::endl;
+    std::cout << "actions: grey, rotate90, blur, rotate180, flip, resize, canny, morphology" << std::endl;
+}
 int main(int argc, char **argv)
 {
 
-    if (argc < 4)
+    if (argc < 4 || argc > 6 || argv[1] == "--help" || argv[1] == "-h")
     {
-        std::cout << "usage:" << argv[0] << " <image path> <changed_image path> <action>" << std::endl;
-        std::cout << "actions: grey , rotate90 ,blur , rotate180, flip, resize, canny, morphology" << std::endl;
+        printHelp(argv[0]);
         return -1;
     }
 
     std::string imagePath = argv[1];
+
+    if (!isValidPath(imagePath))
+    {
+        std::cout << "Error: The image path doesn't exist." << std::endl;
+        return -1;
+    };
+
     std::string action = argv[3];
     std::string changed_image_path = argv[2];
-
-    if (argv[2] == "--help" || argv[2] == "-h")
+    std::string changed_image_directory = changed_image_path.substr(0, changed_image_path.rfind('/'));
+    if (!isValidPath(changed_image_directory))
     {
-        std::cout << "usage:" << argv[0] << " <image path> <changed_image path> <action>" << std::endl;
-        std::cout << "actions: grey , rotate90 ,blur , rotate180, flip, resize, canny, morphology" << std::endl;
-        return 0;
+        if (!providePath(changed_image_directory))
+        {
+            return -1;
+        }
     }
 
     cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
@@ -115,9 +157,20 @@ int main(int argc, char **argv)
 
     Actions(image, changed_image, argc, argv);
 
+    while (isValidPath(changed_image_path))
+    {
+        int pos = changed_image_path.find('.');
+        if (changed_image_path[pos - 1] == ')')
+        {
+            changed_image_path[pos - 2] += 1;
+        }
+        else
+        {
+            changed_image_path.insert(pos, "(1)");
+        }
+    }
     cv::imwrite(changed_image_path, changed_image);
-    cv::waitKey(0);
+    cv::waitKey(1000);
     cv::destroyAllWindows();
-
     return 0;
 }
