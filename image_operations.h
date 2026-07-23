71135fdcd7080a06772d2b888eef2dc7256f36da
Gor Mkrtchyan <gorm4063@gmail.com>
2026-07-24 00:00:05 +0400

    move all functions from start.cpp

--- image_operations.h
+++ image_operations.h
@@ -13,4 +13,22 @@ namespace OpencvFunctions
     void applyCanny(const cv::Mat &input, cv::Mat &output, double threshold1, double threshold2);
     void applyMorphology(const cv::Mat &input, cv::Mat &output, int operation, int kernelSize);
 }
+namespace Utils
+{
+    void checkHelpFlag(int argc, char **argv);
+    void printHelp(const std::string &programName);
+    void handleInvalidFileExtension(const std::set<std::string> &validFileExtensions);
+    bool isValidFileExtension(const std::string &filePath, const std::set<std::string> &validExtensions);
+    bool isValidPath(const std::string &path);
+    void coutCaseInvalidPath(const std::string &path);
+    void handleDublicateFileName(std::string &changed_image_path);
+    void exitCaseImageEmpty(const cv::Mat &image);
+    void exitCaseInvalidFileExtension(const std::string &imagePath);
+    void coutCaseInvalidFileExtension(const std::string &imagePath, const std::set<std::string> &validFileExtensions);
+    void exitCaseInvalidPath(const std::string &changed_image_path);
+    bool askUserToCreateDirectory();
+    bool handleMissingDirectory(std::string &changed_image_directory);
+
+}
+
 #endif
